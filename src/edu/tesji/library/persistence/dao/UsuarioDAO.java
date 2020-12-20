309e166e3e13f2e019c323cfaf847d8a0137676a
RosarioZavala <chayo.zavala@hotmail.com>
2020-12-20 16:16:02 -0600

    refactor: actualización de UsuarioDAO
    Se renombra método selectusuario a selectAllUsuario
    Se agrega método selectUsuario y consulta para poder autenticar desde el
    controller de login.

--- src/edu/tesji/library/persistence/dao/UsuarioDAO.java
+++ src/edu/tesji/library/persistence/dao/UsuarioDAO.java
@@ -16,8 +16,10 @@ public class UsuarioDAO {
 	private PreparedStatement prepStatement;
 	private ResultSet resultSet;
 	private static final String QUERY_SELECT_ALL_AUTOR = "SELECT idusuario, nombre, password, status FROM usuario";
+	private static final String QUERY_SELECT_AUTOR = "SELECT idusuario, nombre, password, status FROM usuario"
+			+ " WHERE nombre = ? AND password";
 
-	public List<Usuario> selectusuario() {
+	public List<Usuario> selectAllUsuario() {
 
 		List<Usuario> usuarioList = new ArrayList<Usuario>();
 		Connection connection = null;
@@ -31,6 +33,38 @@ public class UsuarioDAO {
 				Usuario usuario = new Usuario(resultSet.getInt("idusuario"), resultSet.getString("nombre"),
 						resultSet.getString("password"), resultSet.getBoolean("status"));
 				usuarioList.add(usuario);
+			}
+		} catch (SQLException e) {
+			LOG.error("SQLException", e);
+
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					LOG.error("SQLException", e);
+				}
+			}
+		}
+		return usuarioList;
+	}
+	
+	public List<Usuario> selectUsuario(Usuario usuario) {
+
+		List<Usuario> usuarioList = new ArrayList<Usuario>();
+		Connection connection = null;
+		try {
+			DBConnection dbConnection = new DBConnection();
+			connection = dbConnection.getConnection();
+			prepStatement = connection.prepareStatement(QUERY_SELECT_AUTOR);
+			prepStatement.setString(1, usuario.getNombre());
+			prepStatement.setString(2, usuario.getPassword());
+			resultSet = prepStatement.executeQuery();
+
+			while (resultSet.next()) {
+				Usuario usuarioRow = new Usuario(resultSet.getInt("idusuario"), resultSet.getString("nombre"),
+						resultSet.getString("password"), resultSet.getBoolean("status"));
+				usuarioList.add(usuarioRow);
 			}
 		} catch (SQLException e) {
 			LOG.error("SQLException", e);
