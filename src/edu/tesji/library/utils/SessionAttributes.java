5b479dd907ce5ed12dcfa2fc9e3c9c7a772223f1
RosarioZavala <chayo.zavala@hotmail.com>
2020-12-25 11:06:57 -0600

    refactor: se agrega clase de constantes.
    Para poder tener el nombre de atributos de sesión, u otras cosas.

---
+++ src/edu/tesji/library/utils/SessionAttributes.java
@@ -0,0 +1,9 @@
+package edu.tesji.library.utils;
+
+public class SessionAttributes {
+	
+	/**
+	 * Contendr&aacute; el nombre de nuestro usuario autenticado.
+	 */
+	public static final String USER_ADMIN_NAME = "user_admin_name";
+}
