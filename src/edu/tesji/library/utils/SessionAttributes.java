18ffed580ea4cea3db861b2903d334949de0c20d
RosarioZavala <chayo.zavala@hotmail.com>
2021-01-15 18:58:36 -0600

    refactor: Se agrega parámetros a la clase SessionAttributes

--- src/edu/tesji/library/utils/SessionAttributes.java
+++ src/edu/tesji/library/utils/SessionAttributes.java
@@ -11,16 +11,17 @@ public class SessionAttributes {
 	 * Nombre del action para iniciar la carga de una pantalla.
 	 */
 	public static final String ACTION_INIT = "init";
-	
+
 	/**
 	 * Nombre del action para guardar registro.
 	 */
 	public static final String ACTION_SAVE = "save";
-	
-	//Nombre del action para eliminar registro
+
+	// Nombre del action para eliminar registro
 	public static final String ACTION_DELETE = "delete";
-	//Nombre del action para actualizar registro
-			public static final String ACTION_UPDATE ="update";
-	//Nombre del action para limpiar campos
-			public static final String ACTION_CLEAN = "clean";
+	// Nombre del action para actualizar registro
+	public static final String ACTION_UPDATE = "update";
+	// Nombre del action para limpiar campos
+	public static final String ACTION_CLEAN = "clean";
+	public static final String ACTION_FIND = "find";
 }
